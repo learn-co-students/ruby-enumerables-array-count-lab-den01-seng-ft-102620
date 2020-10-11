b40cea172e6f341e9ef62bad6929b76029d9ebe3
Javaria Brascom <javariab17@gmail.com>
2020-10-11 19:55:46 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,28 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  cont=0
+  index=0
+while index< array.count do
+      if  array[index].class == String
+         cont+=1  
+    
+  end
+   index+=1 
 end
-
-def count_empty_strings(array)
+ cont
+ end
+ 
+ 
+  def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+    cont=0
+  index=0
+  while index< array.count do
+      if  array[index] == ""
+         cont+=1  
+    
+  end
+   index+=1 
+end
+ cont
 end
